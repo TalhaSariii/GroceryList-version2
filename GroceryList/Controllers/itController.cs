6f50648af783a228fdc22437a141e838dc18babe
Talha Sarı <117735845+TalhaSariii@users.noreply.github.com>
2023-07-26 15:27:53 +0300

    temp ile çalışan hali

--- GroceryList/Controllers/itController.cs
+++ GroceryList/Controllers/itController.cs
@@ -9,11 +9,14 @@ namespace GroceryList.Controllers
     {
         private readonly Context c = new Context();
 
-        public IActionResult Index(int userId, int page = 1, int pageSize = 5)
-        {
-            var values = c.Items.Where(x => x.IsDeleted == false && x.UserId == userId).ToList();
-            return View(values.ToPagedList(page, pageSize));
-        }
+         public IActionResult Index(int userId, int page = 1, int pageSize = 5)
+          {
+           var values = c.Items.Where(x => x.IsDeleted == false && x.UserId == userId).ToList();
+
+           TempData["UserId"] = userId; 
+
+        return View(values.ToPagedList(page, pageSize));
+           }
 
         [HttpGet]
         public IActionResult login()
@@ -55,36 +58,39 @@ namespace GroceryList.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
-
         [HttpGet]
-        public IActionResult EditItem(int Id)
+        public IActionResult EditItem(int Id, int userId)
         {
-            var item = c.Items.FirstOrDefault(x => x.Id == Id);
+            var item = c.Items.FirstOrDefault(x => x.Id == Id && x.UserId == userId);
             if (item != null)
             {
                 item.IsEditing = true;
                 c.SaveChanges();
             }
-            return RedirectToAction("Index");
+            return RedirectToAction("Index", new { userId = userId, editing = true }); 
         }
+
+
+
 
         [HttpPost]
         public IActionResult EditItem(Item d)
         {
-            var it = c.Items.Find(d.Id);
-            it.Name = d.Name;
-            it.Type = d.Type;
-            it.Amount = d.Amount;
-            it.Price = d.Price;
-            it.ShopName = d.ShopName;
-            it.ModifiedDate = d.ModifiedDate;
-
-
-            it.IsActive = Request.Form["IsActive"] == "on";
-
-            it.IsEditing = false;
-            c.SaveChanges();
-            return RedirectToAction("Index");
+            var it = c.Items.FirstOrDefault(x => x.Id == d.Id && x.UserId == d.UserId);
+            if (it != null)
+            {
+                it.Name = d.Name;
+                it.Type = d.Type;
+                it.Amount = d.Amount;
+                it.Price = d.Price;
+                it.ShopName = d.ShopName;
+                it.ModifiedDate = d.ModifiedDate;
+                it.IsActive = Request.Form["IsActive"] == "on";
+                it.IsEditing = false;
+                c.SaveChanges();
+            }
+            return RedirectToAction("Index", new { userId = d.UserId });
         }
+
     }
 }
